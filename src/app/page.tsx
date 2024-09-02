03f0d246975ae264474876be1611f0aeae943f00
Oussama_ezitouni <Oussama.ezitouni@intelcia.com>
2024-09-02 20:47:28 +0100

    feat: Update title and description in metadata
    - Updated the title and description in the metadata of the RootLayout component.
    - The new title is "Mr Technologies" and the new description is "Generated by Oussama Ezitouni & Mohamed El Machhoune".

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import dynamic from "next/dynamic";
 
-const Scene = dynamic(() => import("@/components/Scene"), {
+const Scene = dynamic(() => import("@/components/Scene/index.jsx"), {
   ssr: false,
 });
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-black">
+    <main className="flex min-h-screen">
       <Scene />
     </main>
   );
--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -1,50 +1,160 @@
-import React, { useRef } from "react";
-import { MeshTransmissionMaterial, useGLTF, Text } from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
-import { useControls } from "leva";
+import React, { useEffect, useState } from "react";
+import { useGLTF, Float, Text } from "@react-three/drei";
+import { useTransform } from "framer-motion";
+import { motion } from "framer-motion-3d";
+import { useThree } from "@react-three/fiber";
 
-export default function Model() {
-  const { nodes } = useGLTF("/medias/Bordeaux.glb");
+export default function Model({ mouse }) {
+  const [activeShape, setActiveShape] = useState(1);
   const { viewport } = useThree();
-  const torus = useRef(null);
 
-  useFrame(() => {
-    torus.current.rotation.y += 0.02;
-  });
+  useEffect(() => {
+    setTimeout(() => {
+      if (activeShape == 11) {
+        setActiveShape(1);
+      } else {
+        setActiveShape(activeShape + 1);
+      }
+    }, 2000);
+  }, [activeShape]);
 
+  const { nodes } = useGLTF("/medias/floating_shapes4.glb");
   return (
-    <>
-      <Text
-        font={"/fonts/PPNeueMontreal-Bold.otf"}
-        position={[0, 0, 0]}
-        fontSize={1.5}
-        anchorX="center"
-        anchorY="middle"
-        color="black"
-        // fillOpacity={0.5}
-      >
-        Mr Technologies
-      </Text>
-      <group ref={torus} dispose={null}>
-        <group scale={0.1}>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.SurfaceLaptop1.geometry}
-            material={nodes.SurfaceLaptop1.material}
-            position={[-0.338, 8, -10.84]}
-            rotation={[-Math.PI / 3, 0, 0]}
+    <group dispose={null}>
+      <group>
+        <Text
+          scale={viewport.width / 5}
+          font={"/fonts/PPNeueMontreal-Bold.otf"}
+          position={[0, 0, -1]}
+          fontSize={0.5}
+          color="#194178"
+          anchorX="center"
+          anchorY="middle"
+        >
+          Mr Technologies!
+        </Text>
+        <group>
+          <Mesh
+            node={nodes.Sphere001}
+            multiplier={2.4}
+            mouse={mouse}
+            isActive={activeShape == 1}
           />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.KeyboardFabric2.geometry}
-            material={nodes.KeyboardFabric2.material}
-            position={[-0.338, -8, 0]}
-            rotation={[-Math.PI / 3, 0, 0]}
+          <Mesh
+            node={nodes.Sphere002}
+            multiplier={2.4}
+            mouse={mouse}
+            isActive={activeShape == 2}
+          />
+          <Mesh
+            node={nodes.Cylinder002}
+            multiplier={1.2}
+            mouse={mouse}
+            isActive={activeShape == 3}
+          />
+          <Mesh
+            node={nodes.Sphere003}
+            multiplier={1}
+            mouse={mouse}
+            isActive={activeShape == 4}
+          />
+          <Mesh
+            node={nodes.Cylinder003}
+            multiplier={1.8}
+            mouse={mouse}
+            isActive={activeShape == 5}
+          />
+          <Mesh
+            node={nodes.Cylinder005}
+            multiplier={1.8}
+            mouse={mouse}
+            isActive={activeShape == 6}
+          />
+          <Mesh
+            node={nodes.Cube002}
+            multiplier={2}
+            mouse={mouse}
+            isActive={activeShape == 7}
+          />
+          <Mesh
+            node={nodes.Cylinder006}
+            multiplier={1.2}
+            mouse={mouse}
+            isActive={activeShape == 8}
+          />
+          <Mesh
+            node={nodes.Cylinder007}
+            multiplier={1.6}
+            mouse={mouse}
+            isActive={activeShape == 9}
+          />
+          <Mesh
+            node={nodes.Cylinder009}
+            multiplier={1.8}
+            mouse={mouse}
+            isActive={activeShape == 10}
+          />
+          <Mesh
+            node={nodes.Sphere}
+            multiplier={1.5}
+            mouse={mouse}
+            isActive={activeShape == 11}
           />
         </group>
       </group>
-    </>
+    </group>
+  );
+}
+
+useGLTF.preload("/medias/floating_shapes4.glb");
+
+function Mesh({ node, multiplier, mouse, isActive }) {
+  const { geometry, material, position, scale, rotation } = node;
+  const a = multiplier / 2;
+  const rotationX = useTransform(
+    mouse.x,
+    [0, 1],
+    [rotation.x - a, rotation.x + a]
+  );
+  const rotationY = useTransform(
+    mouse.y,
+    [0, 1],
+    [rotation.y - a, rotation.y + a]
+  );
+  const positionX = useTransform(
+    mouse.x,
+    [0, 1],
+    [position.x - multiplier * 2, position.x + multiplier * 2]
+  );
+  const positionY = useTransform(
+    mouse.y,
+    [0, 1],
+    [position.y + multiplier * 2, position.y - multiplier * 2]
+  );
+
+  const getRandomMultiplier = () => {
+    return Math.floor(Math.random() * 2) * (Math.round(Math.random()) ? 1 : -1);
+  };
+
+  return (
+    <Float>
+      <motion.mesh
+        castShadow={true}
+        receiveShadow={true}
+        geometry={geometry}
+        material={material}
+        position={position}
+        rotation={rotation}
+        scale={scale}
+        rotation-y={rotationX}
+        rotation-x={rotationY}
+        position-x={positionX}
+        position-y={positionY}
+        animate={{
+          rotateZ: isActive ? rotation.z + getRandomMultiplier() : null,
+        }}
+        transition={{ type: "spring", stiffness: 75, damping: 100, mass: 3 }}
+      />
+    </Float>
   );
 }
