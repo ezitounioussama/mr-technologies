64d1882c5fd0539b6bdda7359c55993e9b31fa1a
Oussama_ezitouni <Oussama.ezitouni@intelcia.com>
2024-09-04 23:31:25 +0100

    chore: Update Theme mode-toggle with z-index for better stacking order

--- src/Theme/mode-toggle.tsx
+++ src/Theme/mode-toggle.tsx
@@ -21,7 +21,7 @@ export function ModeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="z-[5000]">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
