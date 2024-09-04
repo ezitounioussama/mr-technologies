9fbb775a39b833c119e39d8c2267027346e84ad0
Oussama_ezitouni <Oussama.ezitouni@intelcia.com>
2024-09-04 23:29:25 +0100

    feat: Update Hero component with improved design and content

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,3 +1,12 @@
+import { Cover } from "../../components/ui/cover";
+
 export function Hero() {
-  return <></>;
+  return (
+    <div>
+      <h1 className="text-4xl space-y-2 md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        Unbeatable Deals, Delivered
+        <br /> at <Cover>the Speed of Light</Cover>
+      </h1>
+    </div>
+  );
 }
