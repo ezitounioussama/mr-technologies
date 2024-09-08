315450b56f0a49879a1b3e88fcf186700ecdc479
Oussama_ezitouni <Oussama.ezitouni@intelcia.com>
2024-09-08 21:57:29 +0100

    Refactor MrTechnologiesLaptop component and remove unused code

--- src/components/Slider/MrTechnologiesLaptop.tsx
+++ src/components/Slider/MrTechnologiesLaptop.tsx
@@ -1,32 +1,9 @@
-import { Link } from "react-router-dom";
 import { MacbookScroll } from "../ui/mr-technologies-scroll";
-import { useTheme } from "../../Theme/theme-provider";
 
 export default function MrTechnologiesLaptop() {
   return (
     <div className="overflow-hidden dark:bg-black bg-white w-full">
-      <MacbookScroll
-        badge={
-          <Link to="https://gotodev.ma">
-            <Badge />
-          </Link>
-        }
-        showGradient={false}
-      />
+      <MacbookScroll />
     </div>
   );
 }
-// Peerlist logo
-const Badge = () => {
-  const { theme } = useTheme();
-
-  return (
-    <>
-      {theme === "dark" ? (
-        <img src="/Gotodev/Logo_White.png" alt="Gotodev" className="w-8 h-8" />
-      ) : (
-        <img src="/Gotodev/Logo_Black.png" alt="Gotodev" className="w-8 h-8" />
-      )}
-    </>
-  );
-};
